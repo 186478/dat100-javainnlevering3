38e9e70823720a637ee8ae982e9c5576e1cc377e
Teodor <teodor.orjansen@gmail.com>
2024-11-12 13:30:54 +0100

    siste push

--- src/main/java/no/hvl/dat100/oppgave4/SkrivBlogg.java
+++ src/main/java/no/hvl/dat100/oppgave4/SkrivBlogg.java
@@ -1,15 +1,27 @@
 package no.hvl.dat100.oppgave4;
 
-import java.io.FileNotFoundException;
-import java.io.PrintWriter;
+import java.io.File;
 
-import no.hvl.dat100.common.TODO;
-import no.hvl.dat100.oppgave3.*;
+import no.hvl.dat100.oppgave3.Blogg;
 
 public class SkrivBlogg {
+	private String mappe;
+	private String filnavn;
 
 	public static boolean skriv(Blogg samling, String mappe, String filnavn) {
 
-		throw new UnsupportedOperationException(TODO.method());
+		try {
+			File myFile = new File("Skrive blogg.txt");
+			
+			if (myFile.createNewFile()) {
+				System.out.println("filen er opretta " + myFile.getName());
+			} else {
+				System.out.println("filen eksisterer allerede");
+			}
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+		return true;
+		
 	}
 }
